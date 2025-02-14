b9410724f437f6896b677181a568432699ffa3f7
Xavierno Yanura Alexander <56068050+Yanura7788@users.noreply.github.com>
2025-02-14 12:55:18 +0800

    Update Speech.jsx

--- src/components/hero/Speech.jsx
+++ src/components/hero/Speech.jsx
@@ -12,13 +12,10 @@ const Speech = () => {
         <TypeAnimation
           sequence={[
             1000,
-            "Hello! My name is Xavierno Yanura Alexander, and I am a passionate web and game developer. With a strong background in both frontend and backend technologies, I specialize in creating dynamic and user-friendly web applications. In addition to web development, I am also an avid game developer. I create immersive gaming experiences using game engines like Unity and Unreal Engine. I have a solid understanding of game mechanics, AI programming, and graphics rendering, which allows me to build engaging games for both desktop and mobile platforms.
-My goal is to combine my technical skills and creativity to build seamless, interactive experiences—whether it’s through websites or games. I’m always excited to learn new technologies and take on challenging projects that push my boundaries as a developer.
-If you’re interested in collaborating or discussing a potential project, feel free to reach out!",
+            "Hello! My name is Xavierno Yanura Alexander, and I am a passionate web and game developer. With a strong background in both frontend and backend technologies",
             1000,
-            "Hello! My name is Xavierno Yanura Alexander, and I am a passionate web and game developer. With a strong background in both frontend and backend technologies, I specialize in creating dynamic and user-friendly web applications. In addition to web development, I am also an avid game developer. I create immersive gaming experiences using game engines like Unity and Unreal Engine. I have a solid understanding of game mechanics, AI programming, and graphics rendering, which allows me to build engaging games for both desktop and mobile platforms.
-My goal is to combine my technical skills and creativity to build seamless, interactive experiences—whether it’s through websites or games. I’m always excited to learn new technologies and take on challenging projects that push my boundaries as a developer.
-If you’re interested in collaborating or discussing a potential project, feel free to reach out! ",
+            "I specialize in creating dynamic and user-friendly web applications. In addition to web development, I am also an avid game developer. I create immersive gaming experiences using game engines like Unity and Unreal Engine. I have a solid understanding of game mechanics, AI programming, and graphics rendering, which allows me to build engaging games for both desktop and mobile platforms.
+My goal is to combine my technical skills and creativity to build seamless, interactive experiences—whether it’s through websites or games.",
             1000,
           ]}
           wrapper="span"
